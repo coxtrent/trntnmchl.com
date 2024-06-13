6294a2e7d17bb8ff5fc10a9e122a2ff33e8ddf86
coxtrent <coxtrent@umich.edu>
2024-06-13 16:55:57 -0400

    i'm gonna try squashing these commits after i fix this shit

--- trntnmchl/before_request.py
+++ trntnmchl/before_request.py
@@ -9,7 +9,7 @@ import trntnmchl
 
 @trntnmchl.app.before_request
 def before_request():
-    if request.url.startswith('http//'):
+    if request.url.startswith('http://'):
         url = request.url.replace('http://', 'https://', 1)
         code = 301
         return redirect(url, code=code)
