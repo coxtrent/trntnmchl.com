bd455cdcc5e9043efb354f7fa6a0102e656ee9c4
coxtrent <coxtrent@umich.edu>
2024-06-13 17:27:04 -0400

    omfg idk wtf is wrong with this shit

--- trntnmchl/before_request.py
+++ trntnmchl/before_request.py
@@ -9,7 +9,14 @@ import trntnmchl
 
 @trntnmchl.app.before_request
 def before_request():
+    """Redirect to HTTPS and remove www."""
+    url = None
     if request.url.startswith('http://'):
         url = request.url.replace('http://', 'https://', 1)
         code = 301
+    if request.url.startswith('https://www.'):
+        url = request.url.replace('https://www.', 'https://', 1)
+        code = 301
+
+    if url is not None:
         return redirect(url, code=code)
