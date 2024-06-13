0f0020f46373823e92ff79622280c0c3acaac03e
coxtrent <coxtrent@umich.edu>
2024-06-13 16:22:05 -0400

    damn it, once again

--- trntnmchl/before_request.py
+++ trntnmchl/before_request.py
@@ -9,7 +9,7 @@ import trntnmchl
 
 @trntnmchl.app.before_request
 def before_request():
-    if not request.is_secure:
+    if request.url.startswith('http//'):
         url = request.url.replace('http://', 'https://', 1)
         code = 301
         return redirect(url, code=code)
